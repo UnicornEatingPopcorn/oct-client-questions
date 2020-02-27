2c96bb223bbbc4a4f86c94f85fc77845df7b5553
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-02-27 21:39:50 +0900

    install Firebase

--- src/main.js
+++ src/main.js
@@ -3,10 +3,24 @@ import App from "./App.vue";
 import router from "./router/router.js";
 import store from "./store/store.js";
 import VueFuse from "vue-fuse";
+// import firebase from "@/services/FirebaseService"
 
 require("./assets/main.sass");
 Vue.config.productionTip = false;
 Vue.use(VueFuse);
+
+// let vue = null
+// firebase.auth().onAuthStateChanged(async user => {
+//   await store.dispatch("user/setUser", user)
+//
+//   if (!vue) {
+//     vue = new Vue({
+//       router,
+//       store,
+//       render: h => h(App)
+//     }).$mount("#app")
+//   }
+// })
 
 new Vue({
   router,
