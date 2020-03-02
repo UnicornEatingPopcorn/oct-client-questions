a95762f65bfc382d26710fc72e99f15fdc63b1fa
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-03-02 16:07:26 +0900

    add dropdown in order nav-burger work

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,43 @@
 <template lang="pug">
-.header
-  .columns.header__columns.is-marginless
-    .column.is-3.is-offset-1
-      p.header__title Portfolio version of creating travel plan
-    // Responsive toggle
-    //- .column.is-1
-    //-   a.navbar-burger.burger(@mouseover="navbarMenu = !navbarMenu" role="button" aria-expanded="false")
-    //-     span(aria-hidden="true")
-    //-     span(aria-hidden="true")
-    //-     span(aria-hidden="true")
+nav.navbar
+  .container.is-desktop
+    .navbar-brand
+      span.navbar-item
+        p.header__title Portfolio version of creating travel plan
+      span.navbar-burger.burger(@click="isNavbarMenuOpened = !isNavbarMenuOpened" role="button" aria-expanded="false")
+        span(aria-hidden="true")
+        span(aria-hidden="true")
+        span(aria-hidden="true")
     // Menu
-    .column
-      .header__items
-        .header__item
-          router-link.header__item-link(:to="{ name: 'plan-create' }") Create plan 
-        .header__item
-          router-link.header__item-link(:to="{ name: 'plan-list' }") Show all my plans
-        .header__item-button
-          span.header__item-button_title(@click="isSignInModalOpen = true") Sign in
+    .navbar-menu
+      .navbar-end
+        .navbar__items
+          .navbar__item
+            router-link.navbar__item-link(:to="{ name: 'plan-create' }") Create plan 
+          .navbar__item
+            router-link.navbar__item-link(:to="{ name: 'plan-list' }") Show all my plans
+          .navbar__item-button(@click="isSignInModalOpened = true") 
+            span.navbar__item-button_title Sign in
+    
+    .dropdown.is-right(:class="{'is-active': isNavbarMenuOpened}")
+      .dropdown-menu(id="dropdown-menu" role="menu")
+        .dropdown-content
+          .dropdown-item.is-nav-link(@click="isNavbarMenuOpened = false")
+            router-link.dropdown-item__link.is-centered-responsive(:to="{ name: 'plan-create' }") Create plan
+            hr.dropdown-divider
+          .dropdown-item.is-nav-link(@click="isNavbarMenuOpened = false")
+            router-link.dropdown-item__link.is-centered-responsive(:to="{ name: 'plan-list' }") Show all my plans 
+            hr.dropdown-divider
+          .dropdown-item.is-nav-link(@click="closeNavbarDropdown")
+            .dropdown__item-button
+              span.navbar__item-button_title Sign in
 
-    .modal(:class="{ 'is-active': isSignInModalOpen }")
-      .modal-background(@click="isSignInModalOpen = false")
+    .modal(:class="{ 'is-active': isSignInModalOpened }")
+      .modal-background(@click="isSignInModalOpened = false")
       .modal-card
         header.modal-card-head
           p.modal-card-title Please, sign in!
-          button.delete(@click="isSignInModalOpen = false")
+          button.delete(@click="isSignInModalOpened = false")
         section.modal-card-body
            .field
              .control
@@ -36,42 +49,37 @@
                input.input(v-model="password")
         footer.modal-card-foot
           button.button.is-success(@click="saveUser") Save Changes
-          button.button(@click="isSignInModalOpen = false") Cancel
-
-
-
-      //- .dropdown.is-right(:class="{'is-active': navbarMenu}")
-      //-   .dropdown-menu(id="dropdown-menu" role="menu")
-      //-     .dropdown-content
-      //-       .dropdown-item.is-nav-link(v-for="navItem in navItems")
-      //-         router-link.a-navbar.is-centered-responsive(to="navItem.path") {{ navItem.title }}
-  .header__border
+          button.button(@click="isSignInModalOpened = false") Cancel
 </template>
 
 <script>
 export default {
   data() {
     return {
-      navbarMenu: false,
-      isSignInModalOpen: false,
+      isNavbarMenuOpened: false,
+      isSignInModalOpened: false,
       login: "",
       password: ""
     };
   },
   methods: {
     saveUser() {
-      return (this.isSignInModalOpen = false);
+      return (this.isSignInModalOpened = false);
+    },
+    closeNavbarDropdown() {
+      return (
+        (this.isNavbarMenuOpened = false), (this.isSignInModalOpened = true)
+      );
     }
   }
 };
 </script>
 
 <style lang="sass">
-.header
+.navbar
 
   &__title
-    color: #f7b944
-    font-size: 20px
+    font-size: 20px !important
     margin-bottom: 0px
 
   &__items
@@ -112,10 +120,42 @@ export default {
     display: block
     z-index: 3
 
-  &__border
-    border-bottom: 1px solid black
-
   &__columns
     display: flex
     align-items: center
+
+.dropdown
+  display: block !important
+
+.dropdown-item
+  font-size: 1.2rem !important
+  transition: 0.1s linear
+  &:hover
+    transform: scale(1.1)
+
+.dropdown-content
+  width: 300px
+  padding-left: 20px
+  padding-right: 20px
+
+.dropdown-item__link
+  color: white !important
+  transition: all 0.1s linear
+  font-size: 1.2rem
+
+.dropdown__item-button
+  min-width: 130px
+  font-size: 1.2rem !important
+  height: 40px
+  transition: 0.1s linear
+  background-image: linear-gradient(to right bottom, rgba(252,234,187,1) 0%, rgba(248,181,0,1) 47%, rgba(252,205,77,1) 76%, rgba(251,223,147,1) 100%)
+  transform: skewX(-18deg)
+  border-radius: 4px
+  display: flex
+  justify-content: center
+  align-items: center
+
+  &:hover
+    transform: scale(1.1) skewX(-18deg)
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19)
 </style>
