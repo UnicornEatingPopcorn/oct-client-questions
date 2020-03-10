79088b2ca5a792ba890846bfa90f90e66b20fd65
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2020-03-10 16:26:08 +0900

    add AdditionalQuestions to questions in db.json and create new component for showing additional answers

--- src/components/AdditionalQuestionSelect.vue
+++ src/components/AdditionalQuestionSelect.vue
@@ -1,17 +1,23 @@
 <template lang="pug">
 .columns.is-multiple
   .column(v-if="slug===question.slug")   
-    label.base-select__label {{ question.title }}
-    select.base-input
+    label.additional-question-select__label {{ question.title }}
+    select.additional-question-input
       option(
         v-for="option in question.select_options"
         :key="option.id"
         :value="option.name"
         :selected="option.name === null") {{ option.name }}
+  AdditionalAnswers(:question="question")
 </template>
 
 <script>
+import AdditionalAnswers from "@/components/AdditionalAnswers.vue";
+
 export default {
+  components: {
+    AdditionalAnswers
+  },
   props: {
     question: Object,
     slug: String
@@ -20,11 +26,11 @@ export default {
 </script>
 
 <style lang="sass">
-.base-select__label
+.additional-question-select__label
   color: white
   font-size: 14px
 
-.base-input
+.additional-question-input
    background-color: #495057 !important
    color: #f7b944 !important
    border-inline-start: 0px !important
